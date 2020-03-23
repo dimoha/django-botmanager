621324f0bbdefd099b0442c2ed2fb0f24b131b11
Evgeniy Titov <falgore88@gmail.com>
2020-03-23 19:26:03 +0300

    0.1.5

--- botmanager/utils.py
+++ botmanager/utils.py
@@ -1,11 +1,13 @@
 import fcntl
 import logging
 import os
+from django.conf import settings
+
 def management_lock(view_func):
 
     def wrapper_lock(*args, **kwargs):
         try:
-            lock_file_path = "{0}.lock".format(args[0].__class__.__module__.split('.')[-1])
+            lock_file_path = os.path.join(settings.BASE_DIR, "{0}.lock".format(args[0].__class__.__module__.split('.')[-1]))
             f = open(lock_file_path, 'w')
             fcntl.lockf(f, fcntl.LOCK_EX + fcntl.LOCK_NB)
         except IOError:
