f168fd7205727db8f8785ccdc97952c941950459
Evgeniy Titov <falgore88@gmail.com>
2020-03-23 18:54:28 +0300

    0.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-botmanager',
-    version='0.1.1',
+    version='0.1.4',
     description='Async tasks for django',
     author='Dimoha',
     author_email='dimoha@controlstyle.ru',
