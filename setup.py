4ac506799a5431cb8092818d316436079b4ba182
Den4ik-Bro <denis.rr@bk.ru>
2023-11-17 16:31:42 +0300

    change setup.py

--- setup.py
+++ setup.py
@@ -14,4 +14,8 @@ setup(
         "psutil==5.8.0"
     ],
     packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['locale/*/LC_MESSAGES/*.mo', 'locale/*/LC_MESSAGES/*.po'],
+    }
 )
