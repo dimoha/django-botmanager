446a24580b1f8d59bc3043e1827ab0a8dd082e8d
yasinalizade <yasin.alizade@yandex.ru>
2024-01-22 16:01:48 +0300

    https://jira.ly9.ru/browse/GLOBAL-1798

--- botmanager/admin.py
+++ botmanager/admin.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
+import os
+
 from django.contrib import admin
+from django.http import HttpResponseNotFound, FileResponse
+from django.shortcuts import get_object_or_404
+from django.urls import path
 from django.utils.html import escape
 from django.utils.translation import gettext_lazy as _
 from botmanager.models import Task
+from botmanager import settings
+from botmanager.management.commands.bot_manager import Command
 
 
 class TaskAdmin(admin.ModelAdmin):
@@ -10,6 +17,7 @@ class TaskAdmin(admin.ModelAdmin):
                     'create_dt', 'finish_dt', 'error_field', 'attempt_period', 'attempt_count', 'input_field']
     list_filter = ('is_complete', 'is_failed', 'in_process', 'name')
     search_fields = ('id', 'name')
+    change_form_template = 'admin/botmanager/task/change_form.html'
 
     def get_queryset(self, request):
         qs = super(TaskAdmin, self).get_queryset(request)
@@ -36,5 +44,43 @@ class TaskAdmin(admin.ModelAdmin):
         return obj.input
 
     input_field.short_description = _("Вводные данные")
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['change_form_template'] = 'admin/botmanager/task/change_form.html'
+        if request.user.is_staff:
+            extra_context["show_open_logfile_button"] = True
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path("<path:task_id>/open-logfile/",
+                 self.admin_site.admin_view(self.open_logfile),
+                 name="botmanager_task_open_logfile"),
+        ]
+        return custom_urls + urls
+
+    def open_logfile(self, request, task_id):
+        task = get_object_or_404(Task, pk=task_id)
+        imported_class = [
+            Command.import_from_string(i) for i in settings.MAIN_CONFIG["tasks"].keys()
+            if Command.import_from_string(i).name == task.name
+        ]
+
+        if not imported_class:
+            return HttpResponseNotFound()
+
+        filename = imported_class[0].get_log_file_name()
+        dir = settings.MAIN_CONFIG["logs"]["dir"]
+        file_path = os.path.join(dir, filename)
+
+        if os.path.exists(file_path):
+            return FileResponse(open(file_path, "rb"))
+        else:
+            return HttpResponseNotFound()
+
+    open_logfile.short_description = _(u"Открыть файл")
+
 
 admin.site.register(Task, TaskAdmin)
