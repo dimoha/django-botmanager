b659bdf9cbdb5a209de68fec8b2c823060fba592
Yasin Alizade <yasin.alizade@yandex.ru>
2024-05-30 11:09:57 +0300

    Open log file button (#7)
    * https://jira.ly9.ru/browse/GLOBAL-1798
    
    * Add template link to admin.ModelAdmin
    
    * Add template link to admin.ModelAdmin
    
    * Corrected file directory
    
    * Corrected file directory
    
    * Corrected file directory
    
    * Corrected file directory
    
    * Corrected file directory
    
    * Corrected file directory
    
    * Corrected file directory
    
    * Corrected file directory
    
    * change_form template
    
    * Remove decorator
    
    * https://jira.ly9.ru/browse/GLOBAL-1798
    
    * Change location of change_form_template
    
    * Change location of change_form_template
    
    * Change location of change_form_template
    
    * Add MANIFEST to copy lib to project
    
    * Сorrect MANIFEST.in
    
    * Remove condition on template
    
    * Initialized class
    
    * Initialized class
    
    * Edit filename for log file
    
    * Add folder name from class for log file
    
    * Change FileResponse to StreamingHttpResponse
    
    * Add with operator
    
    * Add file_generator

--- botmanager/admin.py
+++ botmanager/admin.py
@@ -2,7 +2,7 @@
 import os
 
 from django.contrib import admin
-from django.http import HttpResponseNotFound, FileResponse
+from django.http import HttpResponseNotFound, StreamingHttpResponse
 from django.shortcuts import get_object_or_404
 from django.urls import path
 from django.utils.html import escape
@@ -54,6 +54,11 @@ class TaskAdmin(admin.ModelAdmin):
         ]
         return custom_urls + urls
 
+    @staticmethod
+    def file_generator(file_name):
+        with open(file_name, "rb") as file:
+            yield from file
+
     def open_logfile(self, request, task_id):
         task = get_object_or_404(Task, pk=task_id)
         imported_class = [
@@ -70,7 +75,7 @@ class TaskAdmin(admin.ModelAdmin):
         file_path = os.path.join(dir, folder, filename) + ".log"
 
         if os.path.exists(file_path):
-            return FileResponse(open(file_path, "rb"))
+            return StreamingHttpResponse(self.file_generator(file_path), content_type="text/event-stream")
         else:
             return HttpResponseNotFound()
 
